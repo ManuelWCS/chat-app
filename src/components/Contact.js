07fe7b0655bbf8fb5b4e741a146a55729aa0379d
Manuel <robinard.manuel@gmail.com>
2020-10-12 15:33:13 +0200

    second commit with all modifs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,31 @@
-import "Contact.css";
+import React from 'react';
+import './Contact.css';
+
+
+function Contact () {
+    
+    const avatar = "https://randomuser.me/api/portraits/women/64.jpg";
+    const name = "Betty";
+    const status = true; 
+    return (
+        
+        <div className = "Contact">
+            <div>
+          <img className ="avatar" src = {avatar} />
+           </div>
+           <div className="status">
+                <p>{name} </p>
+            <div className ="status-online">
+                </div>
+                <div className ="status-text"> 
+                <p>{status ? 'Online' : "Offline"}</p> 
+                </div>
+               
+                
+                </div>
+            
+        </div>
+    );
+}
+
+export default Contact; 
