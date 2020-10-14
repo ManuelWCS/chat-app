13559f1a674b9e7f83e1af15308a9584761e9270
Manuel <robinard.manuel@gmail.com>
2020-10-14 13:27:34 +0200

    fixed contact list

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types';
 
+const avatar = "https://randomuser.me/api/portraits/women/64.jpg";
+const name = "Betty";
+const status = true; 
 
-function Contact () {
-    
-    const avatar = "https://randomuser.me/api/portraits/women/64.jpg";
-    const name = "Betty";
-    const status = true; 
-    return (
-        
-        <div className = "Contact">
-            <div>
-          <img className ="avatar" src = {avatar} />
-           </div>
-           <div className="status">
-                <p>{name} </p>
-                <div className ="status-online">
-                </div>
-                <div className ="status-text"> 
-                <p>{status ? 'Online' : "Offline"}</p> 
-                </div>
-               
-                
-                </div>
-            
-        </div>
-    );
+function Contact(props){
+    return(
+        <section className = "Contact">
+            <aside>
+                <img className="avatar" src={props.avatar} alt={props.name} />
+            </aside>
+            <main className="status">
+                <h3 className="name">{props.name}</h3>
+                <span className={props.online?"status-online": "status-offline"}></span><h4 className="status-text">{props.online ? "Online" : "Offline"}</h4>
+            </main>
+        </section>
+    )
+}
+             
+
+Contact.propTypes = {
+    name : PropTypes.string.isRequired,
+    avatar : PropTypes.string.isRequired,
+    online : PropTypes.bool
 }
 
 export default Contact; 
